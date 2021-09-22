be5a3e7084e3a67ac59593b792c3488a23c83bd5
klballard <kyleleeballard@gmail.com>
2021-09-22 16:37:13 -0400

    highlighted errors

--- scripts.js
+++ scripts.js
@@ -1,15 +1,15 @@
 const baseURL = 'https://api.dictionaryapi.dev/api/v2/entries/en/';
 let url;
 
-const searchWord = document.getElementById('search');
-const resultDisplay = document.getElementById('displayResults');
-const form = document.getElementById('form');
+const searchWord = document.getElementById('search'); //!
+const resultDisplay = document.getElementById('displayResults'); //!
+const form = document.getElementById('form'); //!
 
-form.addEventListener('submit', findMeaning);
+form.addEventListener('submit', findMeaning); //!
 
 
-async function findMeaning(e) {
-    e.preventDefault();
+async function findMeaning(e) { //!
+    e.preventDefault(); //!
     url = `${baseURL}${searchWord.value}`
     const response = await fetch(url);
     if (response.status === 404){
@@ -25,8 +25,8 @@ async function findMeaning(e) {
 let displayMeaning = (data) => {
      console.log(data);
 
-     while (resultDisplay.firstChild) {
-        resultDisplay.removeChild(resultDisplay.firstChild);
+     while (resultDisplay.firstChild) { //!
+        resultDisplay.removeChild(resultDisplay.firstChild); //!
     }
     // console.log(data[0]);
     // console.log(data[0].meanings);
@@ -37,13 +37,13 @@ let displayMeaning = (data) => {
     
 
     let def = data;
-    const numberOfDefs = def.length;
+    const numberOfDefs = def.length; //!
     console.log(numberOfDefs);
    
 
     if (def.length === 0 || def.length == undefined){
         console.log('no results');
-        resultDisplay.innerHTML = `
+        resultDisplay.innerHTML = ` //!
         <h4>No result found. Check your spelling, please!</h4>`
     } else {
         for (let i = 0; i < numberOfDefs; i++) {
@@ -56,13 +56,13 @@ let displayMeaning = (data) => {
             let phonLink = document.createElement('a');
             let current = def[i];
             
-            defText.setAttribute('id', 'defText');
+            defText.setAttribute('id', 'defText'); //!
             orgText.setAttribute('id', 'orgText');
             defDiv.setAttribute('id', 'defDiv');
 
 
-            defHead.textContent = 'Definition:';
-            defText.textContent = current.meanings[0].definitions[0].definition;
+            defHead.textContent = 'Definition:'; //!
+            defText.textContent = current.meanings[0].definitions[0].definition; //!
 
             orgHead.textContent = 'Origin:';
             orgText.textContent = current.origin;
